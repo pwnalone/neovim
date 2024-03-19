88fd12c0f09c91ca48b32d3f4dc485cb8e221cb4
Joshua Inscoe <jinscoe123@gmail.com>
2024-03-19 12:52:06 -0700

    Hide bufferline on starter page

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -2,6 +2,7 @@ return {
   -- Always show the bufferline and center Neo-tree's title text.
   {
     "akinsho/bufferline.nvim",
+    event = "BufEnter",
     opts = function(_, opts)
       opts.options.always_show_bufferline = true
       opts.options.offsets[1].text = "Neotree"
